9160d16069919b707fef24545fe02608f091827c
zahra-d <61844084+zahra-d@users.noreply.github.com>
2020-04-17 12:44:54 +0300

    Add files via upload

---
+++ login.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+public class Login {
+public void run() {
+    Scanner scan = new Scanner (new File("the\\dir\\myFile.extension"));
+    Scanner keyboard = new Scanner (System.in);
+    String user = scan.nextLine();
+    String pass = scan.nextLine(); 
+
+    String inpUser = keyboard.nextLine();
+    String inpPass = keyboard.nextLine(); 
+
+    if (inpUser.equals(user) && inpPass.equals(pass)) {
+        System.out.print("welcome");
+    } else {
+        System.out.print("error");
+    }
+}
+} 
